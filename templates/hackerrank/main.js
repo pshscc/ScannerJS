8b5960590961d3de679cb637e40e93d2774e6b7c
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2020-06-09 11:52:01 -0500

    updated templates

--- templates/hackerrank/main.js
+++ templates/hackerrank/main.js
@@ -32,96 +32,64 @@ init: {
 const scanner = (source) => {
     const NUMBER_REGEX = /^(\+|-)?\d+((\.\d+)?(e(\+|-)\d+)?)?$|^(\+|-)?\.\d+(e(\+|-)\d+)?$/i;
     let buffer;
-    let bufferPointer = 0;
-    let bufferIndex = 0;
     if (typeof source === 'string') {
-        buffer = [source];
+        buffer = source;
     }
     else if (Array.isArray(source)) {
-        buffer = source.slice();
+        buffer = source.slice().join('');
     }
     else {
         throw new TypeError('Input must be a string or an Array.<string>');
     }
-    const next = () => {
-        let res = '', count = 0, cur;
-        do {
-            cur = read();
-            count++;
-        } while (isWhitespace(cur));
-        while (cur && !isWhitespace(cur)) {
-            res += cur;
-            cur = read();
-            count++;
+    const next = (retainElement) => {
+        if (!buffer)
+            return null;
+        const regex = /^\s*([^\s]+)/;
+        const ret = regex.exec(buffer);
+        if (ret) {
+            if (!retainElement)
+                buffer = buffer.replace(regex, '');
+            return ret[1];
         }
-        count = Math.max(0, count - 1);
-        if (cur)
-            bufferPointer--;
-        else if (res === '')
-            return { value: null, charactersRead: count };
-        return { value: res, charactersRead: count };
+        if (!retainElement)
+            buffer = '';
+        return ret;
     };
-    const nextLine = () => {
-        let res = '', count = 1, cur = read();
-        while (cur && cur !== '\r' && cur !== '\n') {
-            res += cur;
-            cur = read();
-            count++;
+    const nextLine = (retainElement) => {
+        if (!buffer)
+            return null;
+        const regex = /^([^\r\n]*)(?:\r\n|\n|$)/;
+        const ret = regex.exec(buffer);
+        if (ret) {
+            if (!retainElement)
+                buffer = buffer.replace(regex, '');
+            return ret[1];
         }
-        if (cur === '\r') {
-            count++;
-            cur = read();
-        }
-        if (cur === null)
-            count--;
-        if (res === '') {
-            return { value: cur === '\n' ? res : null, charactersRead: count };
-        }
-        return { value: res, charactersRead: count };
+        if (!retainElement)
+            buffer = '';
+        return null;
     };
     const nextNumber = (retainElement) => {
-        const res = next();
-        if (retainElement)
-            bufferPointer -= res.charactersRead;
-        if (typeof res.value === 'string' && NUMBER_REGEX.test(res.value))
-            return parseFloat(res.value);
+        const res = next(retainElement);
+        if (typeof res === 'string' && NUMBER_REGEX.test(res))
+            return parseFloat(res);
         else
             throw new TypeError('Not a number');
     };
-    const read = () => {
-        if (!buffer.length)
-            return null;
-        let EOF = bufferIndex === buffer.length - 1 && bufferPointer === buffer[bufferIndex].length;
-        while (bufferPointer < 0) {
-            bufferIndex--;
-            bufferPointer += buffer[bufferIndex].length;
-        }
-        if (!EOF && bufferPointer === buffer[bufferIndex].length) {
-            bufferIndex++;
-            bufferPointer = 0;
-            EOF = bufferIndex === buffer.length - 1 && bufferPointer === buffer[bufferIndex].length;
-        }
-        if (EOF)
-            return null;
-        return buffer[bufferIndex][bufferPointer++];
-    };
-    const isWhitespace = (cur) => cur === ' ' || cur == '\n' || cur == '\r';
     return {
         next() {
-            return next().value;
+            return next(false);
         },
         hasNext() {
-            const obj = next();
-            bufferPointer -= obj.charactersRead;
-            return typeof obj.value === 'string';
+            const obj = next(true);
+            return typeof obj === 'string';
         },
         nextLine() {
-            return nextLine().value;
+            return nextLine(false);
         },
         hasNextLine() {
-            const obj = nextLine();
-            bufferPointer -= obj.charactersRead;
-            return typeof obj.value === 'string';
+            const obj = nextLine(true);
+            return typeof obj === 'string';
         },
         nextNumber() {
             return nextNumber(false);
